1844f333f65046a4d45272da4e539e0b28a1198c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-06 20:32:37 +0000

    Deploy Eeenkeeei/homework_notifications to github.com/Eeenkeeei/homework_notifications.git:gh-pages

--- js/app.js
+++ js/app.js
@@ -11,14 +11,15 @@ async function request() {
 }
 
 request();
-
+let timerCoefficient = 60000;
+let constantlyCoefficient = 60000;
 const timerFormEl = document.querySelector('#timer');
 const timerTimeEl = document.querySelector('#timerTime');
 const timerHeaderEl = document.querySelector('#timerHeader');
 const timerTextEl = document.querySelector('#timerText');
 timerFormEl.addEventListener('submit', async (evt) => {
     evt.preventDefault();
-    const timerTime = timerTimeEl.value.trim() * 60000;
+    const timerTime = timerTimeEl.value.trim() * timerCoefficient;
     const timerHeader = timerHeaderEl.value.trim();
     const timerText = timerTextEl.value.trim();
 
@@ -42,13 +43,13 @@ const constantlyButtonEl = document.querySelector('#constantlyButton');
 let counter = 0;
 constantlyButtonEl.addEventListener('click', async (evt) => {
     evt.preventDefault();
-    const constantlyTime = constantlyTimeEl.value.trim() * 60000;
+    const constantlyTime = constantlyTimeEl.value.trim() * constantlyCoefficient;
     const constantlyHeader = constantlyHeaderEl.value.trim();
     const constantlyText = constantlyTextEl.value.trim();
     const constantlyCount = Number(constantlyCountEl.value.trim());
     console.log(constantlyCount);
-    if (constantlyCount === 0 || constantlyCount === '') {
-       let infiniteNotification = setInterval(() => {
+    if (constantlyCount === 0 || constantlyCount == '') {
+        setInterval(() => {
 
             const notification = new Notification(constantlyHeader, {
                 body: constantlyText,
@@ -60,25 +61,48 @@ constantlyButtonEl.addEventListener('click', async (evt) => {
             console.log('infinite notification');
         }, constantlyTime);
     } else {
-       let notificationCount = setInterval(()=>{
+        let notificationCount = setInterval(() => {
             if (constantlyCount === counter) {
                 counter = 0;
                 clearInterval(notificationCount);
                 return;
             }
             counter++;
-           const notification = new Notification(constantlyHeader, {
-               body: constantlyText,
-               icon: 'https://classroom.loftschool.com/pluginfile.php/7719/course/overviewfiles/JavaScript-logo.png',
-               requireInteraction: true,
-               silent: false,
-               tag: 'node' // название тега, любая строка, если она одинаковая, уведомления с одинаковым тегом заменяют существующее
-           });
-           console.log('notification');
+            const notification = new Notification(constantlyHeader, {
+                body: constantlyText,
+                icon: 'https://classroom.loftschool.com/pluginfile.php/7719/course/overviewfiles/JavaScript-logo.png',
+                requireInteraction: true,
+                silent: false,
+                tag: 'node' // название тега, любая строка, если она одинаковая, уведомления с одинаковым тегом заменяют существующее
+            });
+            console.log('notification');
 
         }, constantlyTime);
 
     }
-
+});
+const timerLabelEl = document.querySelector('#timerSwitchLabel');
+const switchTimerEl = document.querySelector('#timerSwitch');
+switchTimerEl.addEventListener('change', (evt) => {
+    if (timerCoefficient === 60000) {
+        timerCoefficient = 1000;
+        timerLabelEl.textContent = 'Время в секундах'
+    } else {
+        timerCoefficient = 60000;
+        timerLabelEl.textContent = 'Время в минутах'
+    };
+    console.log(timerCoefficient)
 });
 
+const constantlyLabelEl = document.querySelector('#constantlySwitchLabel');
+const switchConstantlyEl = document.querySelector('#constantlySwitch');
+switchConstantlyEl.addEventListener('change', (evt) => {
+    if (constantlyCoefficient === 60000) {
+        constantlyCoefficient = 1000;
+        constantlyLabelEl.textContent = 'Время в секундах'
+    } else {
+        constantlyCoefficient = 60000;
+        constantlyLabelEl.textContent = 'Время в минутах'
+    };
+    console.log(constantlyCoefficient)
+});
